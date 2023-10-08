71611792364e5ed7baf5bad197a842c162e57d89
Damilare A <132821778+TheDamiofLagos@users.noreply.github.com>
2023-10-08 03:30:46 +0100

    Add files via upload

---
+++ Notebooks/arithmetic.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add(x, y):
+    return x + y
+
+def minus(x, y):
+    return x - y
+
+def mult(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
+def mod(x, y):
+    return x % y
+
+
+# In[ ]:
+
+
+
+
